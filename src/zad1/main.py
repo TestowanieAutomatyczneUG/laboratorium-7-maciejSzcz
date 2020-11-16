a3e6b0258373ca12556911b4f4bc96c39ccad262
MaciejSzczesny <yetek.lol@gmail.com>
2020-11-16 17:09:52 +0100

    changed zad1 to password validator

--- src/zad1/main.py
+++ src/zad1/main.py
@@ -1,17 +1,40 @@
-class FizzBuzz:
-    def game(self, num):
+import re
+import string
+import distutils
 
-        if type(num) != int:
-            raise Exception("Not a valid number")
-        if ((num % 15) == 0):
-            return "FizzBuzz"
-        elif ((num % 3) == 0):
-            return "Fizz"
-        elif ((num % 5) == 0):
-            return "Buzz"
+class ValidPassword:
+    def validate(self, password):
+        if type(password) != str:
+            raise TypeError()
+        elif len(password) < 8:
+            return False
+        elif re.search('[0-9]', password) is None:
+            return False
+        elif re.search('[A-Z]', password) is None:
+            return False
+        elif re.search(f'[{string.punctuation}]', password) is None:
+            return False
         else:
-            raise Exception("Error in FizzBuzz")
-
+            return True
 
 if __name__ == "__main__":
+    temp = ValidPassword()
+
+    def test_parametrized_from_file():
+        f = open('./data/test_zad1', 'r+')
+        temp_password = ValidPassword()
+
+        for line in f:
+            if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
+                continue
+            else:
+                values = line.split(" ")
+                input_val, result = (values[0], values[1].strip("\n"))
+                if result == "True":
+                    result = True
+                elif result == "False":
+                    result = False
+                print(temp_password.validate(input_val) == result)
+
+    test_parametrized_from_file()
     pass
--- tests/test_1.py
+++ tests/test_1.py
@@ -1,18 +1,23 @@
-from zad1.main import FizzBuzz
+from zad1.main import ValidPassword
 import unittest
 
 
 class TestSimple(unittest.TestCase):
     def test_parametrized_from_file(self):
         f = open('./data/test_zad1', 'r+')
-        temp_FizzBuzz = FizzBuzz()
+        temp_password = ValidPassword()
 
         for line in f:
             if line.startswith("#") or line.startswith(" ") or line.startswith("\n"):
                 continue
             else:
                 values = line.split(" ")
-                result, input_val= (values[0], int(values[1].strip("\n")))
-                print(result)
-                self.assertEqual(temp_FizzBuzz.game(input_val), result)
+                input_val, result = (values[0], values[1].strip("\n"))
+                if result == "True":
+                    result = True
+                elif result == "False":
+                    result = False
+
+                self.assertEqual(temp_password.validate(input_val), result)
+
         f.close()
